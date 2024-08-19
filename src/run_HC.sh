fbc2f8aacfafdbb588cbfd33ef8a525e345852bc
Joao Pedro Araujo Ferreira Campos <jpafcampos@gmail.com>
2024-08-19 11:01:22 -0300

    update

--- src/run_HC.sh
+++ src/run_HC.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run the HC algorithm
-python3 hill_climbing.py --num_runs 20 --data "insurance" --type_exp 3 --random 1 --no-verbose --tabu 100
+python3 hill_climbing.py --num_runs 20 --data "child" --type_exp 3 --random 1 --no-verbose --tabu 100
